50181e9f1d399e1ae365c22e247f51d3dd294e38
Soz <sozraheem@gmail.com>
2026-02-27 13:14:40 +0100

    Fix the creation of TableClient: works now

--- api/src/index.js
+++ api/src/index.js
@@ -1,35 +1,28 @@
 const { app } = require('@azure/functions');
-const { TableClient } = require("@azure/data-tables"); // Table Storage SDK
+const { TableClient } = require("@azure/data-tables"); 
+
+const tableName = "sampletable";
+const client = TableClient.fromConnectionString(
+    process.env.DATABASE_CONNECTION_STRING,
+    tableName
+);
 
 app.setup({
     enableHttpStream: true,
 });
 
-// template
-// app.http('FunctionName', {
-//     methods: ['GET'],
-//     authLevel: 'anonymous',
-//     handler: async (request, context) => {
-//         // logic comes here
-//         return { status: 200, jsonBody: result };
-//     }
-// });
+console.log(process.env.DATABASE_CONNECTION_STRING);
 
-//
 app.http('GetItems', {
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (req, ctx) => {
-        // const client = TableClient.fromConnectionString(
-        //     process.env.AZURE_STORAGE_CONNECTION_STRING,
-        //     "sampletable"
-        // );
         const items = [];
-        items.push("test_item1")
-        items.push("test")
-        // for await (const entity of client.listEntities()) {
-        //     items.push(entity);
-        // }
+        // items.push("test_item1")
+        // items.push("test")
+        for await (const entity of client.listEntities()) {
+            items.push(entity);
+        }
         return { status: 200, jsonBody: items };
     }
 });
@@ -41,10 +34,6 @@ app.http('SubmitItem', {
     authLevel: 'anonymous',
     handler: async (req, ctx) => {
         const body = await req.json();
-        const client = TableClient.fromConnectionString(
-            process.env.AZURE_STORAGE_CONNECTION_STRING,
-            "sampletable"
-        );
         const entity = {
             partitionKey: "itemstest",
             rowKey: Date.now().toString(),
@@ -55,5 +44,4 @@ app.http('SubmitItem', {
         return { status: 200, jsonBody: { success: true } };
     }
 });
-
 
