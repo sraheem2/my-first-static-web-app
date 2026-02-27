57fa3bdf736145ffe3e0a69d4840f74487f4eaaa
Soz <sozraheem@gmail.com>
2026-02-27 16:53:12 +0100

    Add working submit form

---
+++ src/submitmessage.php
@@ -0,0 +1,19 @@
+<?php
+
+var_dump($_SERVER["REQUEST_METHOD"]);
+
+
+// <html>
+//     <head>
+//         <title>Submitted</title>
+//         <link rel="stylesheet" href="styles.css">
+//     </head>
+//     <body>
+//         <h3>The form has been submitted successfully</h3>
+//         &nbsp;
+//         <!-- $fruitname=$_POST['fruitname'];
+//         echo "You have added the fruit: " . $fruitname;
+//         $colorname=$_POST['colorname'];
+//         echo " with the color: " . $colorname; -->
+//     </body>
+// </html>    
---
+++ src/submitscript.js
@@ -0,0 +1,10 @@
+const submit_button = document.getElementById("submitbutton")
+
+submit_button.addEventListener("click", () => {
+    alert ('Form is submitted!')
+}
+)
+
+function submit_function () {
+    alert ('The submit function works')
+}
