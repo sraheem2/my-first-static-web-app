31a48dbfad5ccb95c96e73119619d105f1614c1c
Soz <sozraheem@gmail.com>
2026-03-06 11:45:07 +0100

    Remove test

--- swa-db-connections/test.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  const conn = process.env.SQL_CONNECTION_STRING;
-  context.res = {
-    status: 200,
-    body: { connection: conn ? "VISIBLE" : "MISSING" }
-  };
-};
